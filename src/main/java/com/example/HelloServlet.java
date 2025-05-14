22989134cce4eb1b0f6c7d736537b1151bcf2c01
janakniraula <janakniraula@users.noreply.github.com>
2025-05-14 19:12:44 +0000

    Initial Commit

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, World from Simple Webapp!");
+    }
+}
